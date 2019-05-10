3f48426192dc24eccd4c8506886b79b12b6553ca
Harinath <reddy.harinath10@gmai.com>
2019-05-10 15:00:20 +0200

    Learning

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -6,7 +6,7 @@ class Message extends Component
     {
         super()
         this.state={
-            message:'Welcome visitor'
+            message:'Welcome Harinath'
         }
     }
     changeMessage()
